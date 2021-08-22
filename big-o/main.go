91c91b4ad8b4936c967f5eaede466ae1cb3b6bd4
ono5 <kuroneko_head_lord@yahoo.co.jp>
2021-08-22 15:58:29 +0900

    added big-o constant time

--- big-o/main.go
+++ big-o/main.go
@@ -10,6 +10,7 @@ var snacks = []string{
 	"gummy", "crepe", "sable", "gelato", "cookie",
 }
 
+// Big-O = o(n) linear time
 func findCookie(array []string) {
 	now := time.Now()
 	for _, a := range array {
@@ -20,6 +21,14 @@ func findCookie(array []string) {
 	log.Println("Call to find Cooki took", time.Now().Sub(now))
 }
 
+var boxes = []int{0, 1, 2, 3, 4, 5}
+
+// Big-O = o(1) constant time
+func logFirstTowBoxes(boxes []int) {
+	log.Println(boxes[0])
+	log.Println(boxes[1])
+}
+
 func main() {
-	findCookie(snacks)
+	logFirstTowBoxes(boxes)
 }
